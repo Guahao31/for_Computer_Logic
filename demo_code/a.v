0a1df819536e9db1eab07e1786cf055a71c07240
Guahao31 <799695781@qq.com>
2022-09-09 11:20:34 +0800

    update: slides: 赋值

--- demo_code/a.v
+++ demo_code/a.v
@@ -1,9 +1,6 @@
-module sign_extender
-    (
-        input [7:0] original,
-        output [15:0] sign_extended_original
-    );
-
-    assign sign_extended_original = {{8{original[7]}}, original[7:0]};
-
-endmodule
+always @(posedge clk) begin
+  // a初始值为1,b初始值为0
+  a <= b;
+  b <= a;
+  // 结束时，a=0, b=1
+end
