f7bf7bad10d8948628df0074d1fc0628fbd6497d
Guahao31 <799695781@qq.com>
2022-09-15 21:28:27 +0800

    update: slides

--- demo_code/a.v
+++ demo_code/a.v
@@ -1,10 +1,2 @@
-initial begin
-    // $dumpfile("demo1_tb.vcd");
-    // $dumpvars(0, demo1_tb);
-
-    in = 8'b0000_0000;
-    #10; in = 8'b1000_0001;
-    #10; in = 8'b0000_0001;
-    // Other tests
-    #10;
-end
+// input A, B, C; output Out;
+assign Out = A | (B & C);
