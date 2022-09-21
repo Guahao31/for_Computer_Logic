5f8981a87fd7bbd0375d74c347e1085630540b09
Guahao31 <799695781@qq.com>
2022-09-21 22:25:28 +0800

    update: lab4: LampCtrl_sch_tb

--- demo_code/lab_lab4/LampCtrl_sch_tb.v
+++ demo_code/lab_lab4/LampCtrl_sch_tb.v
@@ -1,30 +1,31 @@
+`timescale 1ns / 1ps
+
 module LampCtrl_sch_tb();
+
 // Inputs
-   reg S1;
-   reg S2;
    reg S3;
+   reg S2;
+   reg S1;
 
 // Output
    wire F;
 
+// Bidirs
+
 // Instantiate the UUT
-   lamp_ctrl UUT (
-		.S1(S1), 
-		.S2(S2), 
+   LampCtrl UUT (
+		.F(F), 
 		.S3(S3), 
-		.F(F)
+		.S2(S2), 
+		.S1(S1)
    );
-
 // Initialize Inputs
+  integer i;
 	initial begin
-	S3=0;S2=0; S1=0;#50;
-	           S1=1; #50;
-		 S2=1;S1=0; #50;
-              S1=1; #50;
-    S3=1;S2=0;S1=0; #50;
-              S1=1; #50; 
-         S2=1;S1=0; #50;
-		      S1=1; #50;
-	end
-
+		for(i=0;i<=8;i=i+1)begin
+			{S3,S2,S1} <= i;
+			#50;
+		end
+   end
+	 
 endmodule // LampCtrl_sch_tb
