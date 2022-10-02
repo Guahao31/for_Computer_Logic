cd587ab3e98ae49ce3f29c49125f5451ba3e80f7
Guahao31 <799695781@qq.com>
2022-10-02 16:33:08 +0800

    add: lab_lab6 MyMC14495_HDL & its testbench

---
+++ demo_code/lab_lab6/MyMC14495_HDL.v
@@ -0,0 +1,48 @@
+`timescale 1ns/1ps
+
+module MyMC14495_HDL(
+  input D0, D1, D2, D3,
+  input LE,
+  input point,
+  output reg p,
+  output reg a, b, c, d, e, f, g
+);
+
+  `define MC14495_NUM {D3, D2, D1, D0}
+  `define MC14495_OUT {a, b, c, d, e, f, g}
+
+  always@(*) begin
+    if(1'b0 == LE) begin
+      /* Able to print */
+      // Point
+      p = ~point;
+      // Num(0~9, A~F)
+      case(`MC14495_NUM)
+        4'h0: `MC14495_OUT = 7'b000_0001;
+        4'h1: `MC14495_OUT = 7'b100_1111;
+        4'h2: `MC14495_OUT = 7'b001_0010;
+        4'h3: `MC14495_OUT = 7'b000_0110;
+        4'h4: `MC14495_OUT = 7'b100_1100;
+        4'h5: `MC14495_OUT = 7'b010_0100;
+        4'h6: `MC14495_OUT = 7'b010_0000;
+        4'h7: `MC14495_OUT = 7'b000_1111;
+        4'h8: `MC14495_OUT = 7'b000_0000;
+        4'h9: `MC14495_OUT = 7'b000_0100;
+        4'hA: `MC14495_OUT = 7'b000_1000;
+        4'hB: `MC14495_OUT = 7'b110_0000;
+        4'hC: `MC14495_OUT = 7'b011_0001;
+        4'hD: `MC14495_OUT = 7'b100_0010;
+        4'hE: `MC14495_OUT = 7'b011_0000;
+        4'hF: `MC14495_OUT = 7'b011_1000;
+        default: `MC14495_OUT = 7'bxxx_xxxx;
+      endcase
+
+    end else begin
+      // Print nothing (LE == 1)
+      `MC14495_OUT = 7'b111_1111;
+      p = 1'b1;
+    end
+
+  end
+
+endmodule
---
+++ demo_code/lab_lab6/MyMC14495_HDL_tb.v
@@ -0,0 +1,63 @@
+`timescale 1ns / 1ps
+
+module MyMC14495_HDL_tb();
+
+// Inputs
+  reg D0;
+  reg D1;
+  reg D2;
+  reg D3;
+  reg LE;
+  reg point;
+
+// Output
+  wire p;
+  wire a;
+  wire b;
+  wire c;
+  wire d;
+  wire e;
+  wire f;
+  wire g;
+
+// Instantiate the UUT
+  MyMC14495_HDL UUT (
+  .D0(D0), 
+  .D1(D1), 
+  .D2(D2), 
+  .D3(D3), 
+  .LE(LE), 
+  .point(point), 
+  .p(p), 
+  .a(a), 
+  .b(b), 
+  .c(c), 
+  .d(d), 
+  .e(e), 
+  .f(f), 
+  .g(g)
+  );
+// Initialize Inputs
+  integer i;
+  initial begin
+    $dumpfile("MyMC14495_HDL.vcd");
+    $dumpvars(1, MyMC14495_HDL_tb);
+
+    D3 = 0;
+    D2 = 0;
+    D1 = 0;
+    D0 = 0;
+    LE = 1'b0;
+    point = 0;
+    
+    for (i=0; i<=15; i=i+1) begin
+      {D3,D2,D1,D0}=i;
+      point = i;
+      #50;
+    end
+      
+    #50;
+    LE = 1'b1;
+    #10;
+  end
+endmodule
