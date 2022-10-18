e3fb06f6939dd4e22d09027b0438240d25647dc1
Guahao31 <799695781@qq.com>
2022-10-18 17:46:44 +0800

    update: lab_lab7: top.v:17

--- demo_code/lab_lab7/top.v
+++ demo_code/lab_lab7/top.v
@@ -14,6 +14,6 @@ module top (
   
   // Note: The following port names may be different from yours, please compare and modify the port names below
   // e.g. enable signals of LED may be named *Les* rather than *LES*, then you need to change .LES() to .Les() below
-  disp_num d0(.clk(clk), .num(num), .LES(SW[7:4]), .points(SW[3:0]), .rst(1'b0), .AN(AN[3:0]), .SEGMENT(SEGMENT[7:0]));
+  disp_num d0(.clk(clk), .Hexs(num), .LES(SW[7:4]), .points(SW[3:0]), .rst(1'b0), .AN(AN[3:0]), .SEGMENT(SEGMENT[7:0]));
 
 endmodule
