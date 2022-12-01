d14f2ff2b5992e93bba7d62be42a3d9031467132
Guahao31 <799695781@qq.com>
2022-12-01 11:35:04 +0800

    update: lab11: Top_tb.v

--- demo_code/lab_labB/Top.v
+++ demo_code/lab_labB/Top.v
@@ -28,7 +28,7 @@ module Top(
 
 	wire [31:0] my_clkdiv;
 	wire [2:0] btn_out;
-	reg  [11:0] num;
+	reg  [11:0] num = 12'b0;
 	wire [3:0] A1, A2, B1, B2, C1, C2;
 	wire [3:0] mux_out;
 	wire Co;
@@ -47,9 +47,7 @@ module Top(
 	wire [3:0] reg_B_val = num[ 7: 4];
 	wire [3:0] reg_C_val = num[11: 8];
 
-
-	clkdiv m0(.clk(clk), .rst(1'b0), .clkdiv(my_clkdiv));
-	
+	// clkdiv m0(.clk(clk), .rst(1'b0), .clkdiv(my_clkdiv));
 
 	/* pbdebounce module DELETED for testing purpose */
 	// pbdebounce m1(.clk_1ms(my_clkdiv[17]), .button(BTN_Y[0]), .pbreg(btn_out[0]));
@@ -58,35 +56,68 @@ module Top(
 
 	/* Assign btn_out without debouncing */
 	assign btn_out = BTN_Y;
-	
 
+	// AddSub4b m4(.A(reg_A_val), .B(4'b0001), .Ctrl(A_Ctrl), .S(A1));
+	// AddSub4b m5(.A(reg_B_val), .B(4'b0001), .Ctrl(B_Ctrl), .S(B1));
 
-	AddSub4b m4(.A(reg_A_val), .B(4'b0001), .Ctrl(A_Ctrl), .S(A1));
-	AddSub4b m5(.A(reg_B_val), .B(4'b0001), .Ctrl(B_Ctrl), .S(B1));
+	wire[3:0] A_plus = reg_A_val+4'b1;
+	wire[3:0] A_minus = reg_A_val-4'b1;
+
+	wire[3:0] B_plus = reg_B_val+4'b1;
+	wire[3:0] B_minus = reg_B_val-4'b1;
+
+	assign A1 = A_Ctrl == 1'b0 ? A_plus : A_minus;
+	assign B1 = B_Ctrl == 1'b0 ? B_plus : B_minus;
 	
-	Mux4to1b4_sch m6(.I0(reg_A_val), .I1(reg_B_val), .I2(reg_C_val), .I3(4'b0000),
-									 .s(Trans_select), .o(mux_out));
+	// Mux4to1b4_sch m6(.I0(reg_A_val), .I1(reg_B_val), .I2(reg_C_val), .I3(4'b0000),
+	// 								 .s(Trans_select), .o(mux_out));
+
+	assign mux_out = Trans_select == 2'b00 ? reg_A_val :
+									 Trans_select == 2'b01 ? reg_B_val :
+									 Trans_select == 2'b10 ? reg_C_val :
+									 Trans_select == 2'b11 ? 4'b0000 : 4'bxxxx;
 	
 	/* ALU module implemented in Lab8 */
 	/* A/B	: operands */
 	/* S		: select the operation on ALU  */
 	/* C 		: result of ALU */
 	/* Co		: Carry bit */
-	ALU4b m7(.A(num_A), .B(num_B), .C(ALU_res), .Co(Co), .S(ALU_Ctrl)); // (Co) may be useless
+	ALU4b m7(.A(reg_A_val), .B(reg_B_val), .C(ALU_res), .Co(Co), .S(ALU_Ctrl)); // (Co) may be useless
 	
-	disp_num m8(.clk(clk), .HEXS({reg_A_val, reg_B_val, ALU_res, reg_C_val}), 
-							.LES(4'b0000), .points(4'b0000), .RST(1'b0), .AN(AN),
-							.Segment(SEGMENT));
+	// disp_num m8(.clk(clk), .HEXS({reg_A_val, reg_B_val, ALU_res, reg_C_val}), 
+	// 						.LES(4'b0000), .points(4'b0000), .RST(1'b0), .AN(AN),
+	// 						.Segment(SEGMENT));
 	
 	/* Your code here */
 	// SW[15]: 0 for ALU mode, 1 for Trans mode.
 	assign A2 = (1'b0 == SW[15]) ? A1 : mux_out; 
-	assign B2 = ();
-	assign C2 = ();
+	assign B2 = (1'b0 == SW[15]) ? B1 : mux_out;
+	assign C2 = (1'b0 == SW[15]) ? ALU_res : mux_out;
 	
 	always@(posedge btn_out[0]) num[3:0] = A2;
-	always@() num[7:4] = ();
-	always@() num[11:8] = ();
+	always@(posedge btn_out[1]) num[7:4] = B2;
+	always@(posedge btn_out[2]) num[11:8] = C2;
 	/******************/
+
+endmodule
+
+
+module ALU4b(
+	input[3:0] A,
+	input[3:0] B,
+	input[1:0] S,
+	output[3:0] C,
+	output Co
+);
+
+	wire[4:0] res;
+	assign res = S == 2'b00 ? A+B :
+							 S == 2'b01 ? A-B :
+							 S == 2'b10 ? A&B : 
+							 S == 2'b11 ? A|B :
+							 4'bxxxx;
+
+	assign Co = res[4];
+	assign C = res[3:0];
 
 endmodule
--- demo_code/lab_labB/top_tb.v
+++ demo_code/lab_labB/top_tb.v
@@ -1,16 +1,18 @@
+`timescale 1ns/1ps
+
 module top_tb();
 /* INPUTS */
   reg clk;
   reg[3:0] BTN_Y;
 
   /* SW[1:0] to control if the counter for A or B is reversal */
-	reg A_Ctrl = SW[0];
-	reg B_Ctrl = SW[1];
+	reg A_Ctrl;
+	reg B_Ctrl;
 	/* SW[3:2] to choose the mode of the ALU */
-	reg [1:0] ALU_Ctrl = SW[3:2];
+	reg [1:0] ALU_Ctrl;
 	/* SW[5:4] to choose from A B C and 0 */
 	/* 00 for A; 01 for B; 10 for C; 11 for 0 */
-	reg [1:0] Trans_select = SW[5:4];
+	reg [1:0] Trans_select;
   /* SW[15] choose mode */
   /* 0 for ALU mode, 1 for Trans mode */
   reg mode;
@@ -26,7 +28,7 @@ module top_tb();
   wire[3:0] debug_res;
 
 /* INSTANCE */
-  Top test_m (.clk(clk), .BTN_Y(BTN_Y), .SW(SW_input), debug_A(debug_A),
+  Top test_m (.clk(clk), .BTN_Y(BTN_Y), .SW(SW_input), .debug_A(debug_A),
               .debug_B(debug_B), .debug_C(debug_C), .debug_res(debug_res));
   
 /* CLOCK */
@@ -38,6 +40,8 @@ module top_tb();
   reg[31:0] counter = 0;
 
   initial begin
+    $dumpfile("TOP.vcd");
+    $dumpvars(1, top_tb);
     BTN_Y = 4'b0000;
     A_Ctrl = 1'b0;
     B_Ctrl = 1'b0;
@@ -48,7 +52,7 @@ module top_tb();
 
   always @(negedge clk) begin
     // Reset Btns
-    BTN_Y = 4'b0000;
+    #2 BTN_Y = 4'b0000;
   end
 
   always @(posedge clk) begin
@@ -58,32 +62,35 @@ module top_tb();
 
       /* ALU mode, init A & B with 1 and 2 */
       32'd1: begin
-        BTN_Y = 4'b0001; // A++
+        #2 BTN_Y = 4'b0001; // A++
       end
 
       32'd2, 32'd3, 32'd4: begin
-        BTN_Y = 4'b0010; // B++
+        #2 BTN_Y = 4'b0010; // B++
       end
       // B=3
 
       32'd5: begin
         B_Ctrl = 1'b1;
-        BTN_Y = 4'b0010;
+      end
+
+      32'd6: begin
+        #2 BTN_Y = 4'b0010;
       end
       // B=2
 
       32'd14: begin
-        BTN_Y = 4'b0100; // C = A+B
+        #2 BTN_Y = 4'b0100; // C = A+B
       end
 
       32'd15: begin
         ALU_Ctrl = 2'b01;
-        BTN_Y = 4'b0100; // C = A-B 
+        #2 BTN_Y = 4'b0100; // C = A-B 
       end
 
       32'd16: begin
         ALU_Ctrl = 2'b10;
-        BTN_Y = 4'b0100; // C = A&B
+        #2 BTN_Y = 4'b0100; // C = A&B
       end
 
       32'd17: begin
@@ -92,7 +99,7 @@ module top_tb();
       end
 
       32'd18: begin
-        BTN_Y = 4'b0100; // C = A|B (3)
+        #2 BTN_Y = 4'b0100; // C = A|B (3)
       end
 
       /* Trans mode, Init A=1, B=2, C=3 */
@@ -104,28 +111,28 @@ module top_tb();
       /* C->A */
       32'd31: begin
         Trans_select = 2'b10;
-        BTN_Y = 4'b0001;
+        #2 BTN_Y = 4'b0001;
       end
       /* A=3, B=2, C=3, res=5 */
 
       /* 0->B */
       32'd32: begin
         Trans_select = 2'b11;
-        BTN_Y = 4'b0010;
+        #2 BTN_Y = 4'b0010;
       end
       /* A=3, B=0, C=3, res=3 */
 
       /* C->B */
       32'd33: begin
         Trans_select = 2'b10;
-        BTN_Y = 4'b0010;
+        #2 BTN_Y = 4'b0010;
       end
       /* A=3, B=3, C=3, res=6 */
 
       /* 0->C */
       32'd34: begin
         Trans_select = 2'b11;
-        BTN_Y = 4'b0100;
+        #2 BTN_Y = 4'b0100;
       end
       /* A=3, B=3, C=0, res=6 */
 
@@ -137,7 +144,7 @@ module top_tb();
 
       /* Change A to F(15) */
       32'd41, 32'd42, 32'd43, 32'd44: begin
-        BTN_Y = 4'b0001;
+        #2 BTN_Y = 4'b0001;
       end
       /* A=15, B=3, C=0, res=2 */
 
@@ -148,47 +155,51 @@ module top_tb();
       /* C->C */
       32'd50: begin
         Trans_select = 2'b10;
-        BTN_Y = 4'b0100;
+        #2 BTN_Y = 4'b0100;
       end
 
       /* B->B */
       32'd51: begin
         Trans_select = 2'b01;
-        BTN_Y = 4'b0010;
+        #2 BTN_Y = 4'b0010;
       end
 
       /* A->A */
       32'd52: begin
         Trans_select = 2'b00;
-        BTN_Y = 4'b0001;
+        #2 BTN_Y = 4'b0001;
       end
       /* A=15, B=3, C=0, res=2 */
 
       /* B->C */
       32'd53: begin
         Trans_select = 2'b01;
-        BTN_Y = 4'b0100;
+        #2 BTN_Y = 4'b0100;
       end
       /* A=15, B=3, C=3, res=2 */
 
       /* A->C */
       32'd54: begin
         Trans_select = 2'b00;
-        BTN_Y = 4'b0100;
+        #2 BTN_Y = 4'b0100;
       end
       /* A=15, B=3, C=15, res=2 */
 
       /* A->B */
       32'd55: begin
         Trans_select = 2'b00;
-        BTN_Y = 4'b0010;
+        #2 BTN_Y = 4'b0010;
       end
 
       /* A=15, B=15, C=15, res=14 */
 
-      else begin
+      32'd70: begin
+        $finish();
+      end
+
+      default: begin
         // Clear
-        BTN_Y = 4'b0000;
+        #2 BTN_Y = 4'b0000;
       end
     endcase
   end
